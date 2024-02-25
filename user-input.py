0ee82067ee3d789c8b363326564c7c21d0984c4c
mariemerci10 <160754681+mariemerci10@users.noreply.github.com>
2024-02-25 15:47:59 +0300

    Create user-input.py
    done assignment task

---
+++ user-input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask the user for their name and store it in the variable "name"
+name = input("what is your name?")
+
+# Ask the user for their age and store it in the variable "age"
+age = input("how old are you?")
+
+# Ask the user for their location and store it in the variable "location"
+location = input("where are you located?")
+
+# Print out a personalized message using the user's name, age, and location
+print("Hello {Marie Merci}, you are {20} years old and live in {nairobi}.".format(name, age, location))
