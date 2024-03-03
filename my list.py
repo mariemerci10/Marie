2a50c71068028fffa65707ea62b4774d7dee488c
mariemerci10 <160754681+mariemerci10@users.noreply.github.com>
2024-03-03 16:58:21 +0300

    Add files via upload

---
+++ my list.py
@@ -0,0 +1,29 @@
+list = []
+
+list = [10, 20, 30, 40]
+
+element = int(input("Enter an element to Insert : "))
+position = int(input("Enter position to Insert : "))
+
+print("List before insertion :  ", list)
+
+list.insert(position, element)
+
+print("List after insertion : ", list)
+
+tinylist = [50, 60, 70]
+
+print(tinylist)
+
+element = int(input("Enter an element to remove : "))
+
+
+list.sort(reverse =True)
+
+print(sorted(list))
+
+print(list)
+
+list.index("30")
+
+print(list.index("30"))
